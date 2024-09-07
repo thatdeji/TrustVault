cb6949c44577c8b5160eed08770247cfa4c3f9a0
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-09-07 15:56:41 +0100

    feat : init setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -11,6 +12,10 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      boxShadow: {
+        header:
+          "0px 2px 1px 0px rgba(0, 0, 0, 0.05), 0px 0px 1px 0px rgba(0, 0, 0, 0.25)",
       },
     },
   },
