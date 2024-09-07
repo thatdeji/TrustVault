93da30bca56e3f548d09704b4517969b66aed48f
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-09-07 16:45:25 +0100

    build homepage ui

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,36 +1,73 @@
+"use client";
+import { ArrowRight } from "@/svg/arrow-right";
+import { Check } from "@/svg/check";
+import { Ongoing } from "@/svg/ongoing";
+import Link from "next/link";
+
+const Card: React.FC<{
+  href: string;
+  title: React.ReactNode;
+  icon: React.ReactNode;
+  number: string;
+}> = ({ href, title, icon, number }) => (
+  <Link
+    href={href}
+    className="bg-white min-h-[158px] px-9 card flex-row flex items-center justify-between"
+  >
+    <div className="flex items-center gap-4 justify-between flex-grow">
+      <div className="flex items-center gap-4">
+        <div className="w-6 h-6 flex items-center justify-center">{icon}</div>
+        <h3 className="text-2xl font-normal text-[#1F1F1F]">{title}</h3>
+      </div>
+      <p className="ml-7 text-5xl font-normal text-[#1F1F1F]">{number}</p>
+    </div>
+    <div className="w-[23px] h-[25px]">
+      <ArrowRight />
+    </div>
+  </Link>
+);
+
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
-      <div className="md:col-span-2 bg-white p-6 card">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-6">
+      <div className="lg:col-span-1 bg-white p-6 card">
         <h2 className="text-3xl text-[#1F1F1F] font-medium mb-4">
           Escrow service <br /> made easy
         </h2>
         <p className="text-lg text-[rgba(31,_31,_31,_0.67)] mb-8">
-          You’re a step away from a secure payment means on Blockchain Network
+          You’re a step away from a secure <br /> payment means on Blockchain
+          Network
         </p>
         <button className="button w-fit">Create a deal</button>
       </div>
 
-      <div className="grid grid-cols-1 gap-4">
-        <div className="bg-white min-h-[158px] px-6 card flex items-center justify-between">
-          <div>
-            <h3 className="text-sm text-gray-500">Ongoing deals</h3>
-            <span className="text-4xl font-bold">02</span>
-          </div>
-          <div className="text-xl"> ➔</div>
-        </div>
-
-        <div className="bg-white min-h-[158px] px-6 card flex items-center justify-between">
-          <div>
-            <h3 className="text-sm text-gray-500">Completed deals</h3>
-            <span className="text-4xl font-bold">04</span>
-          </div>
-          <div className="text-xl">{/* Add icon for next/arrow here */} ➔</div>
-        </div>
+      <div className="grid lg:col-span-1 grid-cols-1 gap-4">
+        <Card
+          href="/deals"
+          icon={<Ongoing />}
+          number="02"
+          title={
+            <>
+              Ongoing <br /> deals
+            </>
+          }
+        />
+        <Card
+          href="/deals"
+          icon={<Check />}
+          number="04"
+          title={
+            <>
+              Completed <br /> deals
+            </>
+          }
+        />
       </div>
-      {/* <div className="md:col-span-3 bg-white p-6 card">
-        <h2 className="text-xl font-bold">Recent Activity</h2>
-      </div> */}
+      <div className="lg:col-span-3 px-12  p-6 card">
+        <h2 className="text-3xl text-[#1F1F1F] font-medium ">
+          Recent Activity
+        </h2>
+      </div>
     </div>
   );
 }
---
+++ svg/arrow-right.tsx
@@ -0,0 +1,32 @@
+import { SVGProps } from "react";
+
+export function ArrowRight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="100%"
+      height="100%"
+      viewBox="0 0 23 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_39_674)">
+        <path
+          d="M8.00192 22.921L17.4156 12.3465L7.6665 2.13466"
+          stroke="#5F4E3C"
+          strokeWidth="3"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_39_674">
+          <rect
+            width="24.9474"
+            height="23"
+            fill="white"
+            transform="translate(0 25) rotate(-90)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
